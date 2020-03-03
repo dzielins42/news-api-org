7e20c78653aed2c27dc236397476f5f94c130bb6
dzielins42 <dzielins42@gmail.com>
2020-03-03 17:53:47 +0100

    Simple list screen

--- app/src/main/java/pl/dzielins42/newsapi/MainActivity.kt
+++ app/src/main/java/pl/dzielins42/newsapi/MainActivity.kt
@@ -2,11 +2,35 @@ package pl.dzielins42.newsapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val flexibleAdapter = FlexibleAdapter<NewsItem>(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = flexibleAdapter
+        }
+
+        flexibleAdapter.updateDataSet(getMockData(1, 200), true)
+    }
+
+    private fun getMockData(from: Int, to: Int): List<NewsItem> {
+        return ArrayList<NewsItem>().apply {
+            for (i in from..to) {
+                add(NewsItem("news #$i"))
+            }
+        }
     }
 }
---
+++ app/src/main/java/pl/dzielins42/newsapi/NewsItem.kt
@@ -0,0 +1,45 @@
+package pl.dzielins42.newsapi
+
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_news.*
+
+class NewsItem(
+    private val label: String
+) : AbstractFlexibleItem<NewsItem.NewsItemViewHolder>() {
+
+    override fun bindViewHolder(
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>,
+        holder: NewsItemViewHolder,
+        position: Int,
+        payloads: MutableList<Any>?
+    ) {
+        holder.label.text = label
+    }
+
+    override fun createViewHolder(
+        view: View,
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>
+    ): NewsItemViewHolder {
+        return NewsItemViewHolder(view, adapter)
+    }
+
+    override fun getLayoutRes() = R.layout.item_news
+
+    override fun equals(other: Any?) = label == (other as? NewsItem)?.label
+
+
+    override fun hashCode() = label.hashCode()
+
+    class NewsItemViewHolder(
+        view: View, adapter: FlexibleAdapter<*>
+    ) : FlexibleViewHolder(view, adapter), LayoutContainer {
+        override val containerView: View?
+            get() = itemView
+    }
+}
